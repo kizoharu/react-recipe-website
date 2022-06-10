fe35a32737fc179b0650550795ee4b385befa161
kizoharu <kizoharu@gmail.com>
2022-06-10 18:38:20 +0300

    project complete

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { About } from './pages/About'
+import { Category } from './pages/Category'
+import { Contact } from './pages/Contact'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
+import { Recipe } from './pages/Recipe'
+
+function App() {
+    return (
+        <>
+            <Router>
+                <Header />
+                <main className='container content'>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Home />
+                        </Route>
+                        <Route path='/contact' component={Contact} />
+                        <Route path='/about' component={About} />
+                        <Route path='/category/:name' component={Category} />
+                        <Route path='/meal/:id' component={Recipe} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </main>
+                <Footer />
+            </Router>
+        </>
+    )
+}
+
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
+import { getAllCategories } from '../api'
+import { CategoryList } from '../components/CategoryList'
+import Preloader from '../components/Preloader'
+import { Search } from '../components/Search'
+
+function Home() {
+    const [catalog, setCatalog] = useState([])
+    const [filteredCatalog, setFilteredCatalog] = useState()
+
+    const { pathname, search } = useLocation()
+    const { push } = useHistory()
+
+    const handleSearch = (str) => {
+        setFilteredCatalog(
+            catalog.filter((item) =>
+                item.strCategory.toLowerCase().includes(str.toLowerCase())
+            )
+        )
+        push({
+            pathname,
+            search: `?search=${str}`,
+        })
+    }
+
+    useState(() => {
+        getAllCategories().then((data) => {
+            setCatalog(data.categories)
+            setFilteredCatalog(
+                search
+                    ? data.categories.filter((item) =>
+                          item.strCategory
+                              .toLowerCase()
+                              .includes(search.split('=')[1].toLowerCase())
+                      )
+                    : data.categories
+            )
+        })
+    }, [search])
+
+    return (
+        <>
+            <Search cb={handleSearch} />
+            {!catalog.length ? (
+                <Preloader />
+            ) : (
+                <CategoryList catalog={filteredCatalog} />
+            )}
+        </>
+    )
+}
+
+export { Home }
